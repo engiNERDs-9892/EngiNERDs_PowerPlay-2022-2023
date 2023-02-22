d3bceac453af62c7953db3b2516b9a242e5346ff
engiNERDs <enginerds@tccs.org>
2023-02-21 18:54:03 -0800

    Main Code

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -409,6 +409,8 @@ public class FtcRobotControllerActivity extends Activity
     checkPreferredChannel();
 
     AnnotatedHooksClassFilter.getInstance().callOnCreateMethods(this);
+
+
   }
 
   protected UpdateUI createUpdateUI() {
